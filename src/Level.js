c74d1513142d6d02eb5c98ef6d2f42bb1cbf134f
David Konsumer <konsumer@jetboystudio.com>
2015-11-30 16:11:12 -0800

    added generic events to Level

--- src/Level.js
+++ src/Level.js
@@ -1,11 +1,17 @@
 /**
  * Generic level definition, this should be extended
  */
+import emitonoff from 'emitonoff'
+
 export default class Level {
   constructor (game) {
+    this.onTick = this.onTick.bind(this)
     this.game = game
-    game.on('tick', this.onTick.bind(this))
+    game.on('tick', this.onTick)
+    emitonoff(this)
   }
+
   onTick (dt) {}
+
   destructor () {}
 }
