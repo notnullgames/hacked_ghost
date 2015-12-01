b45b76a934a76a1336786bd4cd09ad6f0fe24d55
David Konsumer <konsumer@jetboystudio.com>
2015-11-30 16:12:13 -0800

    added speech and fullscreen events

--- src/Game.js
+++ src/Game.js
@@ -11,9 +11,77 @@ import TargetCamera from './TargetCamera'
 import Player from './Player'
 import Level1 from './levels/Level1'
 
+/* global responsiveVoice */
+const voice = 'UK English Female'
+// const voice = 'UK English Male'
+// const voice = 'US English Female'
+// const voice = 'Arabic Male'
+// const voice = 'Armenian Male'
+// const voice = 'Australian Female'
+// const voice = 'Brazilian Portuguese Female'
+// const voice = 'Chinese Female'
+// const voice = 'Czech Female'
+// const voice = 'Danish Female'
+// const voice = 'Deutsch Female'
+// const voice = 'Dutch Female'
+// const voice = 'Finnish Female'
+// const voice = 'French Female'
+// const voice = 'Greek Female'
+// const voice = 'Hatian Creole Female'
+// const voice = 'Hindi Female'
+// const voice = 'Hungarian Female'
+// const voice = 'Indonesian Female'
+// const voice = 'Italian Female'
+// const voice = 'Japanese Female'
+// const voice = 'Korean Female'
+// const voice = 'Latin Female'
+// const voice = 'Norwegian Female'
+// const voice = 'Polish Female'
+// const voice = 'Portuguese Female'
+// const voice = 'Romanian Male'
+// const voice = 'Russian Female'
+// const voice = 'Slovak Female'
+// const voice = 'Spanish Female'
+// const voice = 'Spanish Latin American Female'
+// const voice = 'Swedish Female'
+// const voice = 'Tamil Male'
+// const voice = 'Thai Female'
+// const voice = 'Turkish Female'
+// const voice = 'Afrikaans Male'
+// const voice = 'Albanian Male'
+// const voice = 'Bosnian Male'
+// const voice = 'Catalan Male'
+// const voice = 'Croatian Male'
+// const voice = 'Czech Male'
+// const voice = 'Danish Male'
+// const voice = 'Esperanto Male'
+// const voice = 'Finnish Male'
+// const voice = 'Greek Male'
+// const voice = 'Hungarian Male'
+// const voice = 'Icelandic Male'
+// const voice = 'Latin Male'
+// const voice = 'Latvian Male'
+// const voice = 'Macedonian Male'
+// const voice = 'Moldavian Male'
+// const voice = 'Montenegrin Male'
+// const voice = 'Norwegian Male'
+// const voice = 'Serbian Male'
+// const voice = 'Serbo-Croatian Male'
+// const voice = 'Slovak Male'
+// const voice = 'Swahili Male'
+// const voice = 'Swedish Male'
+// const voice = 'Vietnamese Male'
+// const voice = 'Welsh Male'
+// const voice = 'US English Male'
+
 export default class Game {
   constructor () {
     emitonoff(this)
+    this.onKeyDown = this.onKeyDown.bind(this)
+    this.onKeyUp = this.onKeyUp.bind(this)
+    this.onTick = this.onTick.bind(this)
+    this.onResize = this.onResize.bind(this)
+    this.onFullscreen = this.onFullscreen.bind(this)
 
     this.engine = loop(dt => { this.emit('tick', dt) }).start()
 
@@ -23,22 +91,31 @@ export default class Game {
     this.renderer.setClearColor(0x000000, 1)
     document.body.appendChild(this.renderer.domElement)
 
-    // map inputs to emitters & handle tick
-    window.addEventListener('keydown', this.onKeyDown.bind(this))
-    window.addEventListener('keyup', this.onKeyUp.bind(this))
-    this.on('tick', this.onTick.bind(this))
+    this.fullscreenmode = false
+    this.on('tick', this.onTick)
+    this.on('resize', this.onResize)
+    this.on('fullscreen', this.onFullscreen)
+
+    window.addEventListener('keydown', this.onKeyDown)
+    window.addEventListener('keyup', this.onKeyUp)
+    window.addEventListener('resize', () => {
+      this.emit('resize', {width: window.innerWidth, height: window.innerHeight})
+    }, false)
 
     this.scene = new THREE.Scene()
     this.camera = new TargetCamera(35, window.innerWidth / window.innerHeight, 0.1, 10000)
 
     this.player = new Player(this)
     this.level = new Level1(this)
+  }
 
-    window.addEventListener('resize', () => {
-      this.camera.aspect = window.innerWidth / window.innerHeight
-      this.camera.updateProjectionMatrix()
-      this.renderer.setSize(window.innerWidth, window.innerHeight)
-    }, false)
+  say (text) {
+    return new Promise((resolve, reject) => {
+      if (!responsiveVoice.voiceSupport()) {
+        return reject()
+      }
+      responsiveVoice.speak(text, voice, {onend: resolve})
+    })
   }
 
   onKeyUp (ev) {
@@ -49,6 +126,13 @@ export default class Game {
         }
       })
     }
+    if (ev.keyCode === 70) {
+      this.emit('fullscreen')
+    }
+    if (ev.keyCode === 27 && this.fullscreenmode) {
+      this.emit('fullscreen')
+    }
+    console.log(ev.keyCode)
   }
 
   onKeyDown (ev) {
@@ -80,5 +164,36 @@ export default class Game {
     }
     this.camera.update()
     this.renderer.render(this.scene, this.camera)
+  }
+
+  onResize (size) {
+    this.camera.aspect = size.width / size.height
+    this.camera.updateProjectionMatrix()
+    this.renderer.setSize(size.width, size.height)
+  }
+
+  onFullscreen () {
+    this.fullscreenmode = !this.fullscreenmode
+    if (this.fullscreenmode) {
+      if ('webkitCancelFullScreen' in document) {
+        this.renderer.domElement.webkitRequestFullScreen()
+        this.say('full screen mode activated.')
+      } else if ('mozCancelFullScreen' in document) {
+        this.renderer.domElement.mozRequestFullScreen()
+        this.say('full screen mode activated.')
+      } else {
+        this.say('Your device is incapable of full screen mode')
+      }
+    } else {
+      if ('webkitCancelFullScreen' in document) {
+        document.webkitCancelFullScreen()
+        this.say('full screen mode disabled.')
+      } else if ('mozCancelFullScreen' in document) {
+        document.mozCancelFullScreen()
+        this.say('full screen mode disabled.')
+      } else {
+        this.say('Your device is incapable of full screen mode')
+      }
+    }
   }
 }
